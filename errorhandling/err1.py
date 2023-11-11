fa5aa1f7ba6b263c6e3e34120c35e2adc60b4d18
unknown <remahebol@gmail.com>
2023-11-11 22:07:16 +0600

    learnt error

---
+++ errorhandling/err1.py
@@ -0,0 +1,23 @@
+# try
+# except
+# finally
+#try block test a block of code
+#except handles error
+#finally lets you to execute code
+'''try:
+	print(x)
+except:
+	print('An exception has occured!')'''
+#print(x)
+'''try:
+	print(x)
+except NameError:
+	print('varibale x is not defined')
+except:
+	print('Something went wrong!')'''
+try:
+	print('hello')
+except:
+	print('something wrong')
+else:
+	print('nothing wrong')
