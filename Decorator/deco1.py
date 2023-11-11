9de1f0dab8b5908959473c0b6b7abab38b8ab1c7
unknown <remahebol@gmail.com>
2023-11-11 21:35:38 +0600

    Decorator finished

--- Decorator/deco1.py
+++ Decorator/deco1.py
@@ -1,17 +1,25 @@
-#python Decorator
-#Nested function
-def outer (x):
-	def inner(y):
-		return x+y
+#return a function as a value
+'''def greeting(name):
+	def hello():
+		return 'Hello' '+name+'!
+	return hello
+greet=greeting('Hebol')
+print(greet())'''
+'''def make_pretty(func):
+	def inner():
+		print('I got decorated')
+		func()
 	return inner
-add_five=outer(5)
-result=add_five(6)
-print(result)
-#Higher order function
-#pass Function as Argument
-def add(x,y):
-	return x+y
-def cal(func,x,y):
-	return func(4,6)
-result=cal(add,4,6)
-print(result)
+def ordinary():
+	print('I am ordinary')
+# decorate ordinary function
+decorated_func=make_pretty(ordinary)
+decorated_func()'''
+def my_decorator(func):
+	def wrap_func():
+		func()
+	return wrap_func
+@my_decorator
+def hello():
+	print('helloooo')
+hello()
